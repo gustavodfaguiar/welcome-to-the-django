21092f006befc6a57cd977ddf2e0e60a6e1cf9e6
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-06-12 11:51:54 -0300

    Persiste a inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 from django.conf import settings
+
 
 def subscribe(request):
     if request.method == 'POST':
@@ -24,6 +26,8 @@ def create(request):
                 form.cleaned_data['email'],
                 'subscriptions/subscription_email.txt',
                 form.cleaned_data)
+    
+    Subscription.objects.create(**form.cleaned_data)
 
     # Success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
